70a25a54689a8bf514d93d5695e968f04977274c
Raymond Lowe <raymondclowe@gmail.com>
2022-04-09 14:48:45 +0800

    It works!

--- empty_telegram_bot.py
+++ empty_telegram_bot.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 # An empty telegram bot as a template
+#
+# long polling with concurrent.futures so it isn't blocking
 
-
+from ast import Break
 from time import sleep
 import requests
+
+import concurrent.futures
 
 import secrets
 
@@ -18,7 +22,7 @@ class TelegramBot:
         self.offset = 0
         self.previous_offset = -1
         self.offset_filename = "telegram_update_offset.txt"
-        self.get_next_update()
+        # self.get_next_update()
 
     def get_next_update(self):
         if self.offset == 0:
@@ -28,7 +32,8 @@ class TelegramBot:
             except:
                 with open(self.offset_filename, 'w') as f:
                     f.write(str(self.offset))
-        params = {'offset': self.offset + 1, 'limit': 1, 'timeout': 10}
+        # timeout defaults to 30 and has max 50
+        params = {'offset': self.offset + 1, 'limit': 1, 'timeout': 50}
         response = self.session.get(self.api_url + 'getUpdates', params = params)
         if response.status_code != 200:
             return None
@@ -60,21 +65,44 @@ class TelegramBot:
 if __name__ == "__main__":
     # create telegram bot object
     bot = TelegramBot()
-    # loop continuously until keypress or break
-    while True:
-        # wait one second
-        sleep(1)
-        update = bot.get_next_update()
-        if 'message' in update:
-            chat_id = update['message']['chat']['id']
-            first_name = update['message']['from']['first_name']
-            text = update['message']['text']
-            if text == '/start':
-                reply = 'Hello {}({})! I am an empty bot. I will do nothing'.format(first_name,chat_id)
-            else:
-                reply = 'I do not understand you.'
-            bot.send_message(chat_id, reply)
-        bot.save_offset()
+
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers = 10)
+
+    future_update = executor.submit(bot.get_next_update)
+
+    print("Bot started, press Ctrl+C to exit")
+    try:    
+        # loop continuously until keypress or break
+        while True:
+            # wait one second
+            sleep(1)
+            print(". ", end = "")
+
+            # if there is an update
+            if future_update.done():
+                # get the update
+                update = future_update.result()
+                # if the update is a message
+                if 'message' in update:
+                    # get the message
+                    chat_id = update['message']['chat']['id']
+                    first_name = update['message']['from']['first_name']
+                    username = update['message']['from']['username']
+                    text = update['message']['text']
+                    if text == '/start':
+                        reply = 'Hello {}(@{})! I am an empty bot. I will do nothing'.format(first_name,username)
+                    else:
+                        reply = 'I do not understand you.'
+                    bot.send_message(chat_id, reply)
+                bot.save_offset()
+                future_update = executor.submit(bot.get_next_update)
+    # keyboard break exception
+    except KeyboardInterrupt:
+        print("\nKeyboard break")        
+        Break
+
+
+    executor.shutdown(wait = False)
 
 print('done')
 
